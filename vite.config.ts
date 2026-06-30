ec9fd86103ddc374c3a6d5f4b17fde8154f6efe1
Jay <42827142+jay7895214@users.noreply.github.com>
2026-06-30 12:38:01 +0800

    fix: update vite base path and node version for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // 支援 GitHub Pages 任意路徑
+  base: '/better-diagram-as-code-to-image/', // 指定為 GitHub Pages 的專案名稱路徑
 })
